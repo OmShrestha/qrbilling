f023399e8f1235bf9ebe37c794e3291407002ac4
Narahari Sijapati <sijapatiharry@gmail.com>
2020-11-12 22:27:38 +0545

    old order case is remaining

--- src/component/Billing.jsx
+++ src/component/Billing.jsx
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import LogoInfo from './LogoInfo';
 
-import {API_BASE} from '../Constant';
-import {makeStyles} from '@material-ui/core/styles';
+import { API_BASE } from '../Constant';
+import { makeStyles } from '@material-ui/core/styles';
 import {
   Button,
   Divider,
@@ -21,9 +21,10 @@ import StepLabel from '@material-ui/core/StepLabel';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
-import {useHistory} from 'react-router-dom/cjs/react-router-dom.min';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { isGenericTypeAnnotation } from '@babel/types';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
     backgroundImage: `url("https://www.scandichotels.com/imagevault/publishedmedia/qn6infvg30381stkubky/scandic-sundsvall-city-restaurant-verket-10.jpg")`,
   },
@@ -198,12 +199,13 @@ function getSteps() {
   return ['QR CODE', 'ORDER', 'CONFIRM', 'COMPLETE'];
 }
 
-const Billing = (props) => {
-  const {itemTotal, menuList, tableNumber} = props;
+const Billing = props => {
+  const { itemTotal, menuList, tableNumber, companyId } = props;
   const classes = useStyles();
   const [couponeList, setCouponeList] = useState({});
   const [billingInfo, setBillingInfo] = useState({});
   const [hasError, setErrors] = useState(false);
+  const [userDataError, setUserDataError] = useState({});
   // const [orderSaved, setOrderSaved] = useState(false);
   const [activeStep, setActiveStep] = useState(+3);
   const steps = getSteps();
@@ -227,69 +229,91 @@ const Billing = (props) => {
   // }
 
   async function fetchCouponeList() {
-    const requestOptions = {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        phone_number: userData && userData.phoneNumber,
-        email: (userData && userData.email) || '',
-      }),
-    };
-    fetch(
-      API_BASE +
-        'order-user-detail?company=af174b04-b495-47c1-bc32-c0dff7170c34',
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((resCoupone) => setCouponeList(resCoupone))
-      .catch((err) => setErrors(err));
-  }
-
-  async function verifyOrder() {
-    let orderLine = [];
-    Object.keys(itemTotal).map((data) => {
-      let newObj = {
-        id: null,
-        product: '056d497c-6dde-4874-935e-3f91c17a1a06',
-        product_name: itemTotal[data].name,
-        product_code: 'momo12',
-        rate: itemTotal[data].perPlate,
-        quantity: itemTotal[data].number,
-        total: itemTotal[data].total,
-        state: 'New',
-        company: 'af174b04-b495-47c1-bc32-c0dff7170c34',
-        order: '509b9e29-664f-44b8-a984-62235a8bbdca',
+    const error = validatePhoneNumber();
+    setUserDataError(error);
+    if (Object.keys(error).length > 0) {
+    } else {
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          phone_number: userData && userData.phoneNumber,
+          email: (userData && userData.email) || '',
+        }),
       };
-      orderLine.push(newObj);
-    });
-    const requestOptions = {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        company: 'af174b04-b495-47c1-bc32-c0dff7170c34',
-        asset: '2d11f673-12f3-4df3-883e-6a3492a55aea',
-        user: null,
-        name: 'Shyam Shrestha',
-        phone_number: userData && userData.phoneNumber,
-        email: 'shyam@gmail.com',
-        voucher: '964bdedf-2a00-499e-8020-59197597228e',
-        tax: 13.0,
-        bill: null,
-        order_lines: orderLine,
-      }),
-    };
-    fetch(
-      API_BASE + 'company/af174b04-b495-47c1-bc32-c0dff7170c34/order/verify',
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((resCoupone) => setBillingInfo(resCoupone))
-      .catch((err) => setErrors(err));
+      fetch(API_BASE + `order-user-detail?company=${companyId}`, requestOptions)
+        .then(response => response.json())
+        .then(resCoupone => setCouponeList(resCoupone))
+        .catch(err => setErrors(err));
+    }
   }
 
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
+  async function createOrder() {
+    const billingData = billingInfo.data;
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        ...billingData,
+        company: companyId,
+        asset: tableNumber,
+      }),
+    };
+    fetch(API_BASE + `company/${companyId}/order`, requestOptions)
+      .then(response => response.json())
+      .then(resCoupone => (resCoupone.success ? history.push('/Success') : () => {}))
+      .catch(err => setErrors(err));
+  }
+
+  async function verifyOrder(skip) {
+    const error = validateUserInfo();
+    setUserDataError(error);
+    if (!skip && Object.keys(error).length > 0) {
+    } else {
+      let orderLine = [];
+      Object.keys(itemTotal).map(data => {
+        let newObj = {
+          id: null,
+          product: '056d497c-6dde-4874-935e-3f91c17a1a06',
+          product_name: itemTotal[data].name,
+          product_code: null,
+          rate: itemTotal[data].perPlate,
+          quantity: itemTotal[data].number,
+          total: itemTotal[data].total,
+          state: 'New',
+          company: companyId,
+          order: '509b9e29-664f-44b8-a984-62235a8bbdca',
+        };
+        orderLine.push(newObj);
+      });
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          company: companyId,
+          asset: tableNumber,
+          user: couponeList.data.user || null,
+          name: (userData && userData.fullName) || couponeList.data.name || null,
+          phone_number: userData && userData.phoneNumber,
+          email: (userData && userData.email) || couponeList.data.email || null,
+          voucher: (userData && userData.couponeId) || null,
+          tax: 13.0,
+          bill: null,
+          order_lines: orderLine,
+        }),
+      };
+      fetch(API_BASE + 'company/af174b04-b495-47c1-bc32-c0dff7170c34/order/verify', requestOptions)
+        .then(response => response.json())
+        .then(resCoupone => setBillingInfo(resCoupone))
+        .catch(err => setErrors(err));
+    }
+  }
+
+  useEffect(() => {
+    if (couponeList && couponeList.data && couponeList.data.voucher && couponeList.data.voucher.length == 0) {
+      verifyOrder(true);
+    }
+  }, [couponeList]);
 
   const history = useHistory();
   let totalPrice = 0;
@@ -299,16 +323,30 @@ const Billing = (props) => {
     }
   }
 
-  const handleAcordion = (panel) => (event, newExpanded) => {
+  const handleAcordion = panel => (event, newExpanded) => {
     setExpanded(newExpanded ? panel : false);
   };
 
   const handleChange = (event, userData) => {
-    userData = {...userData, [event.target.name]: event.target.value};
+    delete userDataError[event.target.name];
+    userData = { ...userData, [event.target.name]: event.target.value };
     setUserData(userData);
   };
+  const validatePhoneNumber = () => {
+    const errors = {};
+    if (!userData || !userData.phoneNumber) errors.phoneNumber = 'Phone is required';
+    else if (userData.phoneNumber.length < 8) errors.phoneNumber = 'Not valid';
+    return errors;
+  };
+  const validateUserInfo = () => {
+    let errors = {};
+    if (!userData || !userData.fullName) {
+      errors.fullName = 'Name is required';
+    }
+    return errors;
+  };
+  console.log(userData, 'response', couponeList);
 
-  console.log(billingInfo, 'rescopone');
   const serviceCharge = totalPrice * (menuList.data.service_charge / 100) || 0;
   const taxCharge = totalPrice * (menuList.data.tax / 100) || 0;
   const grandTotal = totalPrice + serviceCharge + taxCharge;
@@ -320,7 +358,7 @@ const Billing = (props) => {
         <div className={classes.third}>
           <Grid className={classes.stepperContainer}>
             <Stepper activeStep={activeStep} className={classes.stepper}>
-              {steps.map((label) => (
+              {steps.map(label => (
                 <Step key={label}>
                   <StepLabel className={classes.steps}>{label}</StepLabel>
                 </Step>
@@ -329,30 +367,19 @@ const Billing = (props) => {
             <div>
               {activeStep === steps.length ? (
                 <div>
-                  <Typography className={classes.instructions}>
-                    All steps completed
-                  </Typography>
+                  <Typography className={classes.instructions}>All steps completed</Typography>
                   <Button onClick={handleReset}>Reset</Button>
                 </div>
               ) : null}
             </div>
           </Grid>
           <div className={classes.itemCart}>
-            <Typography className={classes.itemCartTxt}>
-              Items In Cart
-            </Typography>
-            <Typography
-              className={classes.AddItemTxt}
-              onClick={() => props.proceedToRedeem()}
-            >
+            <Typography className={classes.itemCartTxt}>Items In Cart</Typography>
+            <Typography className={classes.AddItemTxt} onClick={() => props.proceedToRedeem()}>
               Add More Items +
             </Typography>
           </div>
-          <Accordion
-            square
-            expanded={expanded === 'panel1'}
-            onChange={handleAcordion('panel1')}
-          >
+          <Accordion square expanded={expanded === 'panel1'} onChange={handleAcordion('panel1')}>
             <AccordionSummary expandIcon={<ExpandMoreIcon />}>
               <div className={classes.processingTxt}>
                 <Typography>Previous Order</Typography>
@@ -370,8 +397,7 @@ const Billing = (props) => {
                 <Grid container className={classes.item} key={menuIndex}>
                   <div className={classes.order}>
                     <Typography className={classes.productName}>
-                      {props.itemTotal[menuData].name}{' '}
-                      {props.itemTotal[menuData].total}
+                      {props.itemTotal[menuData].name} {props.itemTotal[menuData].total}
                     </Typography>
                     <Grid>
                       {
@@ -382,24 +408,20 @@ const Billing = (props) => {
                                 menuData,
                                 '',
                                 props.itemTotal[menuData].perPlate,
-                                props.itemTotal[menuData].name
+                                props.itemTotal[menuData].name,
                               )
                             }
                           >
                             <RemoveIcon />
                           </Button>
-                          <span>
-                            {(props.itemTotal[menuData] &&
-                              props.itemTotal[menuData].number) ||
-                              0}
-                          </span>
+                          <span>{(props.itemTotal[menuData] && props.itemTotal[menuData].number) || 0}</span>
                           <Button
                             onClick={() =>
                               props.addItem(
                                 menuData,
                                 '',
                                 props.itemTotal[menuData].perPlate,
-                                props.itemTotal[menuData].name
+                                props.itemTotal[menuData].name,
                               )
                             }
                           >
@@ -412,7 +434,7 @@ const Billing = (props) => {
 
                   <Divider className={classes.divider} />
                 </Grid>
-              )
+              ),
           )}
 
           <div className={classes.bill}>
@@ -421,25 +443,21 @@ const Billing = (props) => {
               <div>Sub Total</div>
               <div className={classes.price}>
                 Rs.
-                {(billingInfo.data && billingInfo.data.service_charge) ||
-                  totalPrice}
+                {(billingInfo.data && billingInfo.data.service_charge) || totalPrice}
               </div>
             </div>
             <div container className={classes.billing}>
               <div>Restaurant service charge</div>
               <div className={classes.price}>
                 Rs.
-                {(billingInfo.data && billingInfo.data.service_charge) ||
-                  serviceCharge}
+                {(billingInfo.data && billingInfo.data.service_charge_amount.toFixed(2)) || serviceCharge.toFixed(2)}
               </div>
             </div>
             <div className={classes.billing}>
               <div>VAT</div>
               <div className={classes.price}>
                 Rs.
-                {(billingInfo.data &&
-                  billingInfo.data.taxed_amount.toFixed(2)) ||
-                  taxCharge.toFixed(2)}
+                {(billingInfo.data && billingInfo.data.taxed_amount.toFixed(2)) || taxCharge.toFixed(2)}
               </div>
             </div>
             {billingInfo.discount && (
@@ -447,10 +465,7 @@ const Billing = (props) => {
                 <div>Discount</div>
                 <div className={classes.price}>
                   Rs.
-                  {(billingInfo.data &&
-                    billingInfo.data.discount &&
-                    billingInfo.data.discount.toFixed(2)) ||
-                    0}
+                  {(billingInfo.data && billingInfo.data.discount && billingInfo.data.discount.toFixed(2)) || 0}
                 </div>
               </div>
             )}
@@ -459,8 +474,7 @@ const Billing = (props) => {
               <div>Total</div>
               <div className={classes.price}>
                 Rs.
-                {(billingInfo.data && billingInfo.data.grand_total) ||
-                  grandTotal.toFixed(2)}
+                {(billingInfo.data && billingInfo.data.grand_total) || grandTotal.toFixed(2)}
               </div>
             </div>
           </div>
@@ -470,9 +484,7 @@ const Billing = (props) => {
           <Grid className={classes.coupon}>
             {Object.keys(couponeList).length == 0 && (
               <div>
-                <Typography className={classes.cNd}>
-                  Coupon & Discount
-                </Typography>
+                <Typography className={classes.cNd}>Coupon & Discount</Typography>
                 <div className={classes.input}>
                   <TextField
                     variant="outlined"
@@ -480,14 +492,15 @@ const Billing = (props) => {
                     className={classes.txtField}
                     name="phoneNumber"
                     value={(userData && userData.phoneNumber) || ''}
-                    onChange={(e) => handleChange(e, userData)}
+                    onChange={e => handleChange(e, userData)}
                   />
+                  {userDataError && userDataError.phoneNumber && (
+                    <p className="danger" style={{ color: 'red' }}>
+                      {userDataError.phoneNumber}
+                    </p>
+                  )}
                   {Object.keys(couponeList).length == 0 && (
-                    <Button
-                      variant="contained"
-                      className={classes.continue}
-                      onClick={() => fetchCouponeList()}
-                    >
+                    <Button variant="contained" className={classes.continue} onClick={() => fetchCouponeList()}>
                       Continue
                     </Button>
                   )}
@@ -503,15 +516,20 @@ const Billing = (props) => {
                   className={classes.txtField}
                   name="fullName"
                   value={(userData && userData.fullName) || ''}
-                  onChange={(e) => handleChange(e, userData)}
+                  onChange={e => handleChange(e, userData)}
                 />
+                {userDataError && userDataError.fullName && (
+                  <p className="danger" style={{ color: 'red' }}>
+                    {userDataError.fullName}
+                  </p>
+                )}
                 <TextField
                   variant="outlined"
                   placeholder="Email Address"
                   className={classes.txtField}
                   name="email"
                   value={(userData && userData.email) || ''}
-                  onChange={(e) => handleChange(e, userData)}
+                  onChange={e => handleChange(e, userData)}
                 />
                 <Button variant="contained" onClick={() => verifyOrder()}>
                   Continue
@@ -526,7 +544,7 @@ const Billing = (props) => {
                   native
                   className={classes.select}
                   value={(userData && userData.couponeId) || ''}
-                  onChange={(e) => handleChange(e, userData)}
+                  onChange={e => handleChange(e, userData)}
                   placeholder={'Select Coupone'}
                 >
                   <option aria-label="None" value="" />
@@ -551,11 +569,7 @@ const Billing = (props) => {
         )}
         {Object.keys(billingInfo).length != 0 && (
           <Grid className={classes.btnGrid}>
-            <Button
-              variant="contained"
-              className={classes.confirm}
-              onClick={() => history.push('/Success')}
-            >
+            <Button variant="contained" className={classes.confirm} onClick={() => createOrder()}>
               {activeStep === steps.length - 1 ? 'Finish' : 'Confirm Order'}
             </Button>
           </Grid>
--- src/component/ItemDetail.jsx
+++ src/component/ItemDetail.jsx
@@ -1,19 +1,19 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import RedeemForm from './Billing';
-import {API_BASE} from '../Constant';
-import {useHistory} from 'react-router-dom';
+import { API_BASE } from '../Constant';
+import { useHistory } from 'react-router-dom';
 
 // material
 import PropTypes from 'prop-types';
-import {Tabs, Tab, Collapse, Grid} from '@material-ui/core';
-import {makeStyles} from '@material-ui/core/styles';
+import { Tabs, Tab, Collapse, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import {Button} from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import ProductList from './ItemDetail/component/ProductList';
 import LogoInfo from './LogoInfo';
 
 function TabPanel(props) {
-  const {children, value, index, ...other} = props;
+  const { children, value, index, ...other } = props;
   return (
     <div
       role="tabpanel"
@@ -40,7 +40,7 @@ function a11yProps(index) {
   };
 }
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
     backgroundImage: `url("https://www.scandichotels.com/imagevault/publishedmedia/qn6infvg30381stkubky/scandic-sundsvall-city-restaurant-verket-10.jpg")`,
     backgroundColor: '#ECECEC',
@@ -98,7 +98,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const ItemDetails = (props) => {
+const ItemDetails = props => {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
   const [hasError, setErrors] = useState(false);
@@ -122,12 +122,12 @@ const ItemDetails = (props) => {
   async function saveOrder() {
     const requestOptions = {
       method: 'POST',
-      headers: {'Content-Type': 'application/json'},
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(itemTotal),
     };
     fetch(API_BASE + 'company/asset/order/create', requestOptions)
-      .then((response) => response.json())
-      .then((data) => setOrderSaved({postId: data.id}));
+      .then(response => response.json())
+      .then(data => setOrderSaved({ postId: data.id }));
     console.log(orderSaved, 'hehe');
   }
 
@@ -151,10 +151,7 @@ const ItemDetails = (props) => {
   };
 
   const removeItem = (menuIndex, index, price, itemName) => {
-    if (
-      itemTotal[menuIndex + index] &&
-      itemTotal[menuIndex + index].number > 0
-    ) {
+    if (itemTotal[menuIndex + index] && itemTotal[menuIndex + index].number > 0) {
       let newData = {
         ...itemTotal,
         [menuIndex + index]: {
@@ -174,13 +171,11 @@ const ItemDetails = (props) => {
     }
   };
   async function fetchData() {
-    const res = await fetch(
-      API_BASE + 'company/af174b04-b495-47c1-bc32-c0dff7170c34/menu'
-    );
+    const res = await fetch(API_BASE + 'company/af174b04-b495-47c1-bc32-c0dff7170c34/menu');
     res
       .json()
-      .then((res) => setMenuList(res))
-      .catch((err) => setErrors(err));
+      .then(res => setMenuList(res))
+      .catch(err => setErrors(err));
   }
 
   useEffect(() => {
@@ -202,6 +197,7 @@ const ItemDetails = (props) => {
           proceedToRedeem={proceedToRedeem}
           menuList={menuList}
           tableNumber={query.get('table_no')}
+          companyId={props.match.params.id}
         ></RedeemForm>
       ) : (
         <div className={classes.root}>
@@ -217,22 +213,13 @@ const ItemDetails = (props) => {
             {menuList &&
               menuList.data &&
               menuList.data.menu.map((menuData, index) => (
-                <Tab
-                  label={menuData.category_name}
-                  {...a11yProps(index)}
-                  key={index}
-                />
+                <Tab label={menuData.category_name} {...a11yProps(index)} key={index} />
               ))}
           </Tabs>
           {menuList &&
             menuList.data &&
             menuList.data.menu.map((menuData, menuIndex) => (
-              <TabPanel
-                className={classes.panel}
-                key={menuIndex}
-                value={value}
-                index={menuIndex}
-              >
+              <TabPanel className={classes.panel} key={menuIndex} value={value} index={menuIndex}>
                 {menuData.products.map((product, index) => (
                   <ProductList
                     key={index}
@@ -252,10 +239,7 @@ const ItemDetails = (props) => {
 
                       <span>Rs {totalPrice}</span>
                     </div>
-                    <Button
-                      className={classes.checkout}
-                      onClick={() => proceedToRedeem()}
-                    >
+                    <Button className={classes.checkout} onClick={() => proceedToRedeem()}>
                       Check out
                     </Button>{' '}
                   </div>
@@ -263,10 +247,7 @@ const ItemDetails = (props) => {
               </TabPanel>
             ))}
           <Grid container className={classes.orderBtnContainer}>
-            <Button
-              className={classes.orderBtn}
-              onClick={() => proceedToRedeem()}
-            >
+            <Button className={classes.orderBtn} onClick={() => proceedToRedeem()}>
               View Order
             </Button>
           </Grid>

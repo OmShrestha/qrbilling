4892a331a280c3b7b37194e85a0fd25a302faf4e
Narahari Sijapati <sijapatiharry@gmail.com>
2020-11-21 00:37:54 +0545

    Updates

---
+++ precache-manifest.0e0cc06306f5ab4531f6e3654530f790.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55706462a3e5745f4a1c601dc565efe6",
+    "url": "/qrbilling/index.html"
+  },
+  {
+    "revision": "679d9209fd931925f8c3",
+    "url": "/qrbilling/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "183a4cd925de272ed6e4",
+    "url": "/qrbilling/static/css/main.d23b8678.chunk.css"
+  },
+  {
+    "revision": "679d9209fd931925f8c3",
+    "url": "/qrbilling/static/js/2.448892cb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/qrbilling/static/js/2.448892cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "183a4cd925de272ed6e4",
+    "url": "/qrbilling/static/js/main.2c4c11e2.chunk.js"
+  },
+  {
+    "revision": "078d31470e4aa57d23a0",
+    "url": "/qrbilling/static/js/runtime-main.00561f02.js"
+  }
+]);

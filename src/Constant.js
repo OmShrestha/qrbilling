2b99acebeb9340fa8929a9416d40ddf6b6c76063
Prabin Khadka <pravinkhadka32@gmail.com>
2020-11-21 14:32:35 +0545

    build fixes

--- src/Constant.js
+++ src/Constant.js
@@ -4,4 +4,4 @@ export const API_BASE = isProdEnv
   ? 'https://stagingapi.cupponpro.com/api/v1/'
   : 'https://stagingapi.cupponpro.com/api/v1/';
 
-export const BASE_URL = isProdEnv ? 'http://localhost:3000/' : 'http://localhost:3000/';
+export const BASE_URL = isProdEnv ? 'https://mastarqr.com/' : 'http://localhost:3000/';

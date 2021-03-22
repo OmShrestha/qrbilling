9bd79236e13e7ccb39b41911024de733eab777d3
Prabin Khadka <pravinkhadka32@gmail.com>
2021-03-22 12:47:53 +0545

    build env updates

--- src/Constant.js
+++ src/Constant.js
@@ -1,13 +1,11 @@
 const isProdEnv = process.env.NODE_ENV === "production";
 
 export const API_BASE = isProdEnv
-    ? "https://devapi.cupponpro.com/api/v1/"
+    ? "https://api.cupponpro.com/api/v1/"
     : "https://devapi.cupponpro.com/api/v1/";
 
 export const API_BASE_V2 = isProdEnv
-    ? "https://devapi.cupponpro.com/api/v2/"
+    ? "https://api.cupponpro.com/api/v2/"
     : "https://devapi.cupponpro.com/api/v2/";
 
-export const BASE_URL = isProdEnv
-    ? "https://mastarqr.com/"
-    : "http://localhost:3000/";
+export const BASE_URL = process.env.PUBLIC_URL || "http://localhost:3000/";
